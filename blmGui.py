633b9bbb3e44a4e0933970bc44e070837fb6f59d
jphaneuf <phaneuf.joe@gmail.com>
2015-03-16 22:28:31 -0700

    file manager

--- blmGui.py
+++ blmGui.py
@@ -1,27 +1,32 @@
 import sys
 from PyQt4 import QtGui,QtCore
-#box: has filename, full file path,remove button,plotbutton
-#add button
 
 class dataFile(QtGui.QHBoxLayout):
 	def __init__(self):
 		super(dataFile,self).__init__()
-		#text
-		#check box
-		self.rmButton = QtGui.QPushButton("rm")
-		self.addWidget(self.rmButton)
+		self.addStretch(1)
+		self.wg = QtGui.QWidget()
+		self.lbl = QtGui.QLabel("file",self.wg)
+		self.pc = QtGui.QCheckBox("yes",self.wg)		
+		self.addWidget(self.lbl)
+		self.addWidget(self.pc)
 class fileManager(QtGui.QVBoxLayout):
 	def __init__(self):
 		super(fileManager,self).__init__()
 		self.addButton = QtGui.QPushButton("Add File")
+		self.clearButton = QtGui.QPushButton("Clear Files")
 		self.addWidget(self.addButton)
-		myFile = dataFile()
+		self.addWidget(self.clearButton)
 		self.files = []
 		self.addButton.clicked.connect(self.addFile)
+		self.clearButton.clicked.connect(self.removeFiles)
 	def addFile(self):
+		fileName = QtGui.QFileDialog.getOpenFileName(QtGui.QMainWindow(),'Open file','./')
 		x = dataFile()
 		self.addLayout(x)
 		self.files.append(x)
+	def removeFiles(self):
+		self.files = []
 	
 #fm = fileManager()
 #class Example(QtGui.QMainWindow):
@@ -34,7 +39,6 @@ class Example(QtGui.QWidget):
 		fm = fileManager()
 		hbox = QtGui.QHBoxLayout()
 		hbox.addStretch(1)
-		
 		vbox=QtGui.QVBoxLayout()
 		vbox.addStretch(1)
 		vbox.addLayout(hbox)
