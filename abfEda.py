4cb2f1d318c2ae56f55f06297c9b2a6979b82b40
joe <phaneuf.joe@gmail.com>
2015-05-04 19:29:56 -0700

    rearranged integral section for clarity

--- abfEda.py
+++ abfEda.py
@@ -32,22 +32,27 @@ class dataManager:
 		"""time stamps for baseline grabs and integral windows are used
 		to compute the final integral.  These values are grabbed from matplotlib figure.
 		Includes applied voltage entry from user input dialog box"""
-		noiseThreshold = 0.5#Threshold for integration,in nSiemens 
+		noiseThreshold = 0.005#Threshold for integration,in nSiemens 
 		totalIntegral = 0
 		for windowSet in timeStamps:
 			print "time stamps:",windowSet
-			it1,it2,ib,av = [int(x/self.samplePeriod) for x in windowSet] #indices for baselineand integrals
+			it1,it2,ib,av = [int(x/self.samplePeriod) for x in windowSet] #indices for baseline and integrals
 			av = windowSet[3] #applied voltge in mV			
 			print "indices:",it1,it2,ib
 			print "applied Voltage:", av
 			it1,it2 = sorted([it1,it2])#make sure it2>it1
-			#get baseline Conductance @ index
-			baselineConductance = np.mean(self.combinedSignal[max(ib-10,0):min(ib+10,len(self.combinedSignal))])/av
-			integral = self.combinedSignal[it1:it2]/av- baselineConductance
-			#pylab.plot(integral)
-			#pylab.show()
-			integral = [i if i > noiseThreshold else 0 for i in integral] # Clip all below noise
+			br = 10 # baseline range.  Range is mean of signal @ (ib-br to ib+br)
+			#baselineCurrent is current at index provided by baseline slider
+			baselineCurrent = np.mean(self.combinedSignal[max(ib-br,0):min(ib+br,len(self.combinedSignal))]) 
+			"""subset original signal( current in pA) within indices provided by sliders, subtract
+			out the baseline current,then convert to conductance using applied voltage.  Store to 
+			'integral' as intermediate variable.  Note numpy vector math, 'baselineCurrent' gets 
+			subtracted from each element in combinedSignal[it1:it2]"""
+			integral = (self.combinedSignal[it1:it2]-baselineCurrent)/av
+			# Clip all below noise threshold
+			integral = [i if i > noiseThreshold else 0 for i in integral] 
 			integral = np.sum(integral)*self.samplePeriod
+			#sum individual slider windows
 			totalIntegral += float(integral)
 			print "window Integral:",integral
 		print totalIntegral
